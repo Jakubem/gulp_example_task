01984d065643cdbff27d11b0a3ecb889dd9c6df4
Jakubem <jakubmx@gmail.com>
2018-03-10 20:31:51 +0100

    beautify or smthng

--- tasks/pug/pug.js
+++ tasks/pug/pug.js
@@ -1,9 +1,11 @@
 import paths from '../paths';
 
-module.exports = function(gulp, plugins) {
-    return function() {
-        gulp.src(paths.src.pug + '/*.pug')
-            .pipe(plugins.pug({ pretty: false }))
-            .pipe(gulp.dest(paths.dist.html));
-    };
+module.exports = function (gulp, plugins) {
+  return function () {
+    gulp.src(paths.src.pug + '/*.pug')
+      .pipe(plugins.pug({
+        pretty: false
+      }))
+      .pipe(gulp.dest(paths.dist.html));
+  };
 };
